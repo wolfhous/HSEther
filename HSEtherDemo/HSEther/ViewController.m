113600b0fe0b9c0fc9aaf24c3be6e68523503a19
houshuai <e@houshuai.com>
2018-05-17 10:45:09 +0800

    Demo新增微信技术交流群说明

--- HSEtherDemo/HSEther/ViewController.m
+++ HSEtherDemo/HSEther/ViewController.m
@@ -17,7 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //2018-05-17
+    //最近有很多人加我微信，全是问钱包 创建 导入 签名 验签 查询 转账 web3配置 ....
+    //我建了一个微信ios以太坊开发收费群（每人200元），里面有此静态库源文件，我还在群里做技术解答。若感兴趣可以加我微信iHoushuai
     
+    
+    NSLog(@"我建了一个微信ios以太坊开发收费群（每人200元），里面有此静态库源文件，我还在群里做技术解答。若感兴趣可以加我微信iHoushuai");
     // 查询余额 0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0 为eos的token
     //0x0806552bc66a44d9ebe4af007cbb93114c4c0a76 ZAG token
     //0x6f259637dcd74c767781e37bc6133cd6a68aa161 HT  token
@@ -32,6 +37,7 @@
     //创建钱包 等5秒钟，创建比较慢
     [HSEther hs_createWithPwd:@"11111111" block:^(NSString *address, NSString *keyStore, NSString *mnemonicPhrase, NSString *privateKey) {
         NSLog(@"\n\n%@\n%@\n%@\n%@\n\n提供 非静态库源码，包括iOS和Android\n提供 技术支持，包括iOS和Android\n联系wechat： iHoushuai",address,keyStore,mnemonicPhrase,privateKey);
+        NSLog(@"我建了一个微信ios以太坊开发收费群（每人200元），里面有此静态库源文件，我还在群里做技术解答。若感兴趣可以加我微信iHoushuai");
     }];
     
     
@@ -43,6 +49,8 @@
     // 提供 非静态库源码，包括iOS和Android
     // 提供 技术支持，包括iOS和Android
     // 联系wechat： iHoushuai
+    
+    
     
 }
 @end
